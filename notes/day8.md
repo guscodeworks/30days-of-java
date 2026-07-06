733f2d1efb16deaf3137bfb6c978f94f761b0899
guscodeworks <gustavonascimentodasilva23@gmail.com>
2026-07-05 23:04:40 -0300

    day8: Estrutura de repeticao

---
+++ notes/day8.md
@@ -0,0 +1,8 @@
+## O que aprendi
+
+Estrutura de repeticao enquanto(while). Aprendi que essa estrutura de dados so executa uma repeticao se o valor
+for verdadeiro se for falso ela para
+
+# Mini exercicio
+
+no arquivo `EstruturaDeRepeticao.java` fiz um mini exercicio de somar os numeros digitados.
---
+++ src/day8/EstruturaDeRepeticao.java
@@ -0,0 +1,18 @@
+package day8;
+
+import java.util.Scanner;
+
+public class EstruturaDeRepeticao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int soma = 0;
+        while (x != 0) {
+            x = sc.nextInt();
+            soma += x;
+        }
+        System.out.println("Soma total dos números digitados: " + soma);
+        sc.close();
+    }
+}
